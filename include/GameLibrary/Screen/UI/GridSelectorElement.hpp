29a4b38210b4b9eb69a115713e320ba4ad98cf42
lufinkey <luisfinke@gmail.com>
2017-10-09 00:25:45 -0400

    GridSelectorElement

---
+++ include/GameLibrary/Screen/UI/GridSelectorElement.hpp
@@ -0,0 +1,41 @@
+
+#pragma once
+
+#include "ZoomPanElement.hpp"
+#include "ButtonElement.hpp"
+
+namespace fgl
+{
+	class GridSelectorElement : public fgl::ZoomPanElement
+	{
+	public:
+		static const size_t NO_SELECTION = -1;
+		
+		GridSelectorElement();
+		explicit GridSelectorElement(const fgl::RectangleD& frame);
+		virtual ~GridSelectorElement();
+		
+		virtual void layoutChildElements() override;
+		virtual void update(fgl::ApplicationData appData) override;
+		
+		void setItems(const fgl::ArrayList<fgl::ButtonElement*>& items);
+		const fgl::ArrayList<fgl::ButtonElement*>& getItems() const;
+		
+		void setItemSize(const fgl::Vector2d& itemSize);
+		const fgl::Vector2d& getItemSize() const;
+		
+		void setSelectedItemIndex(size_t index);
+		size_t getSelectedItemIndex() const;
+		
+		void setSelectionChangeHandler(const std::function<void()>& func);
+		const std::function<void()>& getSelectionChangeHandler() const;
+		
+	private:
+		fgl::ArrayList<fgl::ButtonElement*> items;
+		fgl::Vector2d itemSize;
+		fgl::Vector2d itemSpacing;
+		fgl::ArrayList<fgl::ButtonElement*> pressedItems;
+		size_t selectedItemIndex;
+		std::function<void()> onSelectionChange;
+	};
+}
---
+++ src/GameLibrary/Screen/UI/GridSelectorElement.cpp
@@ -0,0 +1,158 @@
+
+#include <GameLibrary/Screen/UI/GridSelectorElement.hpp>
+
+namespace fgl
+{
+	GridSelectorElement::GridSelectorElement()
+		: GridSelectorElement(fgl::RectangleD(0,0,300,200))
+	{
+		//
+	}
+	
+	GridSelectorElement::GridSelectorElement(const fgl::RectangleD& frame)
+		: ZoomPanElement(frame),
+		itemSize(100, 100),
+		itemSpacing(10, 10),
+		selectedItemIndex(-1)
+	{
+		//
+	}
+	
+	GridSelectorElement::~GridSelectorElement()
+	{
+		for(auto& item : items)
+		{
+			delete item;
+		}
+	}
+	
+	void GridSelectorElement::update(fgl::ApplicationData appData)
+	{
+		ZoomPanElement::update(appData);
+		
+		size_t prevSelectedItemIndex = selectedItemIndex;
+		for(size_t i=0; i<items.size(); i++)
+		{
+			auto& item = items.get(i);
+			if(item->isPressed())
+			{
+				if(!pressedItems.contains(item))
+				{
+					pressedItems.add(item);
+					if(prevSelectedItemIndex==selectedItemIndex)
+					{
+						selectedItemIndex = i;
+					}
+				}
+			}
+			else
+			{
+				pressedItems.removeEqual(item);
+			}
+		}
+		
+		for(size_t i=0; i<items.size(); i++)
+		{
+			auto& item = items[i];
+			if(i==selectedItemIndex)
+			{
+				item->setSelected(true);
+			}
+			else
+			{
+				item->setSelected(false);
+			}
+		}
+		
+		if(prevSelectedItemIndex != selectedItemIndex)
+		{
+			if(onSelectionChange)
+			{
+				onSelectionChange();
+			}
+		}
+	}
+	
+	void GridSelectorElement::layoutChildElements()
+	{
+		ZoomPanElement::layoutChildElements();
+		
+		if(items.size() > 0)
+		{
+			auto frame = getFrame();
+			auto cols = (unsigned int)(frame.width/(itemSize.x+itemSpacing.x));
+			auto extraSpace = frame.width - ((itemSize.x+itemSpacing.x)*(double)cols);
+			auto spacing = extraSpace / (double)(cols+1);
+			auto objX = (itemSpacing.x/2.0)+spacing;
+			auto objY = (itemSpacing.y/2.0);
+			unsigned int colCounter = 0;
+			for(auto item : items)
+			{
+				item->setFrame(fgl::RectangleD(objX+(itemSpacing.x/2.0), objY, 100.0, 100.0));
+				
+				objX += itemSize.x + itemSpacing.x + spacing;
+				colCounter++;
+				if(colCounter >= cols)
+				{
+					colCounter = 0;
+					objY += itemSize.y + itemSpacing.y;
+				}
+			}
+			if(colCounter!=0)
+			{
+				objY += itemSize.y + itemSpacing.y;
+			}
+			setContentSize(fgl::Vector2d(frame.width, objY));
+		}
+	}
+	
+	void GridSelectorElement::setItems(const fgl::ArrayList<fgl::ButtonElement*>& items_args)
+	{
+		for(auto& item : items)
+		{
+			item->removeFromParentElement();
+		}
+		items = items_args;
+		for(auto& item : items)
+		{
+			addChildElement(item);
+		}
+		layoutChildElements();
+	}
+	
+	const fgl::ArrayList<fgl::ButtonElement*>& GridSelectorElement::getItems() const
+	{
+		return items;
+	}
+	
+	void GridSelectorElement::setItemSize(const fgl::Vector2d& itemSize_arg)
+	{
+		itemSize = itemSize_arg;
+		layoutChildElements();
+	}
+	
+	const fgl::Vector2d& GridSelectorElement::getItemSize() const
+	{
+		return itemSize;
+	}
+	
+	void GridSelectorElement::setSelectedItemIndex(size_t index)
+	{
+		selectedItemIndex = index;
+	}
+	
+	size_t GridSelectorElement::getSelectedItemIndex() const
+	{
+		return selectedItemIndex;
+	}
+	
+	void GridSelectorElement::setSelectionChangeHandler(const std::function<void()>& func)
+	{
+		onSelectionChange = func;
+	}
+	
+	const std::function<void()>& GridSelectorElement::getSelectionChangeHandler() const
+	{
+		return onSelectionChange;
+	}
+}
